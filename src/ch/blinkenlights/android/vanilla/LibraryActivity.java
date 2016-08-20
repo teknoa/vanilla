3c288b3c12f1f8009d56649e8028415cf60e628a
Adrian Ulrich <adrian@blinkenlights.ch>
2016-08-20 13:51:33 +0200

    update tbomb of play release

--- src/ch/blinkenlights/android/vanilla/LibraryActivity.java
+++ src/ch/blinkenlights/android/vanilla/LibraryActivity.java
@@ -159,7 +159,7 @@ public class LibraryActivity
 		int month = cal.get(Calendar.MONTH);
 		int year = cal.get(Calendar.YEAR);
 
-		if (month == 11 && year == 2016) { // Dec 2016
+		if (month == 4 && year == 2017) { // Apr. 2017
 			((TextView) new AlertDialog.Builder(this)
 				.setTitle("Info")
 				.setIcon(android.R.drawable.ic_dialog_info)
